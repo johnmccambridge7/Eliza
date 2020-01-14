9e0e3fb39d50bac69628ac204bfff5d081c9f5ac
johnmccambridge7 <john.l.macdonald.22@dartmouth.edu>
2020-01-14 18:59:44 -0500

    adding spanish version

---
+++ spanish/eliza-lab-spanish.py
@@ -0,0 +1,132 @@
+# I am ([A-Z](.*)) - captures name
+# (I am|I\'m|My name is|I'm called) ([A-Z](.*)) - works for name capture
+# (I am|I\'m)([a-z[:blank:]].*)* ([a-z].*) - generalizes for adjectives
+# (I'm|I am)( an| not| a|[[:blank:]])*([a-z]*)( at|[[:blank:]])?([[:blank:]][A-Za-z]*|[A-Za-z]*)? - extracts person characteristics
+# ([mM]om|[mM]other|[dD]ad|[fF]ather) - extracts parental features
+# I( don't)?( want) to ([A-Za-z](.*)+) ([A-Za-z].*). - want modal verb
+# I (must) ([A-Za-z](.*)+) ([A-Za-z].*). - must modal verb
+# I (can't|can) ([A-Za-z](.*)+) ([A-Za-z].*). - can modal verb
+# (think|hope) - thoughts and aspirations
+
+"""
+Want Test Cases:
+I want to go skiing.
+I don't want to start programming.
+I don't want to learn to ski.
+I don't want to learn how to program.
+I want to go ski.
+"""
+
+import re
+import random
+
+# color codes from: https://godoc.org/github.com/whitedevops/colors
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    WHITE = "\033[97m"
+
+# Spanish Equivalents:
+# Soy ([A-Z](.*)) - captures name
+# (Mi nombre es|Me llamo) ([A-Z](.*)) - works for name capture
+# (Yo |No |Yo no )?([Ee]stoy)([a-z ].*)* ([a-z].*)? - generalize for adjectives
+# (Yo soy |Soy |Yo no soy)(una|not|a|[[:blank:]])*([a-z]*)( de |[[:blank:]])?([[:blank:]][A-Za-z]*|[A-Za-z]*)? - extracts person characteristics
+# ([mM]ama|[mM]adre|[pP]apa)  - parental features
+# (No |Yo )?([Qq]uiero )([A-Za-z](.*)+) ([A-Za-z].*). - want modal verb.
+# (Debo) ([A-Za-z](.*)+) (al|de) ([A-Za-z].*). - must modal verb.
+# (Yo )?([Pp]uedo) ([A-Za-z](.*)+) ([A-Za-z].*). - can modal verb
+# ([Pp]ienso|[Ee]spero) - thoughts and aspirations
+# ([Ss]iempre) - specific examples
+
+# (I\'m |I am )(an |not a[n]? |a | )*([A-Za-z ]*)
+
+greeting = 'Soy ([A-Z](.*))'
+nameCapture = '(Mi nombre es|Me llamo) ([A-Z](.*)+)'
+adjectives = '(Yo |No |Yo no )?([Ee]stoy)([a-z ]*) ([a-z]*)?'
+personal = '(Soy|Yo soy|(Yo )?[nN]o soy)? (una |de | )*([A-Za-z ]*)'
+
+parents = '([mM]om|[mM]other|[dD]ad|[fF]ather)'
+wantModal = 'I( don\'t)?( want) to ([A-Za-z ]*) ([A-Za-z ]*)?'
+mustModal = 'I (must) ([A-Za-z ]*) ([A-Za-z]*)'
+canModal = 'I (can\'t|can) ([A-Za-z]*)([A-Za-z ]*)'
+
+# general cases
+thoughts = '(think|hope)'
+examples = '(always)'
+insults = '(stupid|idiot|loser|sucker)'
+
+border = bcolors.OKBLUE + bcolors.BOLD + " | "
+
+print(border + bcolors.OKGREEN + "***** ELIZA Chat-Bot (español edition) - by John McCambridge *****" + border) 
+
+border = bcolors.OKBLUE + bcolors.BOLD + " | "
+
+while True:
+    text = str(input(border + bcolors.BOLD + bcolors.WHITE + ': '))
+    output = ""
+
+    if text:
+        groupHello = re.search(greeting, text, re.IGNORECASE)
+        groupNameCapture = re.search(nameCapture, text, re.IGNORECASE)
+        adjectiveCapture = re.search(adjectives, text, re.IGNORECASE)
+        parentsCapture = re.search(parents, text, re.IGNORECASE)
+        wantCapture = re.search(wantModal, text, re.IGNORECASE)
+        mustCapture = re.search(mustModal, text, re.IGNORECASE)
+        canCapture = re.search(canModal, text, re.IGNORECASE)
+        personalCapture = re.search(personal, text, re.IGNORECASE)
+        
+        thoughtsCapture = re.search(thoughts, text, re.IGNORECASE)
+        examplesCapture = re.search(examples, text, re.IGNORECASE)
+        insultsCapture = re.search(insults, text,re.IGNORECASE)
+
+        if groupNameCapture:
+            output = "Hola, " + str(groupNameCapture.group(2)) + " - ¿Cómo estás?"
+        elif personalCapture:
+
+            
+            article = "" if personalCapture.group(3) == None else str(personalCapture.group(3))
+            item = str(personalCapture.group(4))
+            conditional = "" if "no" not in str(personalCapture.group(1)) else " no"
+
+            output = "¿Porqué" + conditional + " eres " + article + item + "?"
+
+        elif adjectiveCapture:
+            article = "" if adjectiveCapture.group(1) == None else str(adjectiveCapture.group(1))
+            possessive = " no"
+
+            if "no" not in article:
+                possessive = "" 
+
+            output = "¿Porqué" + possessive + " estás " + str(adjectiveCapture.group(4)).strip() + "?"
+        elif parentsCapture:
+            output = "Tell me more about your " + str(parentsCapture.group(1)) + "."
+        elif wantCapture:
+            conditional = "do" if wantCapture.group(1) == None else "don't"
+            modifier = " " + str(wantCapture.group(4))
+            action = str(wantCapture.group(3)).replace("my", "your")
+            output = "Why " + conditional + " you want to " + action + (modifier if modifier != " " else "") + "?"
+        elif mustCapture:
+            modifier = str(mustCapture.group(2)).replace("my", "your")
+            output = "Why must you " + modifier + " " + str(mustCapture.group(3)) + "?"
+        elif canCapture:
+            endingVerb = "" if str(canCapture.group(3)) == "" else str(canCapture.group(3))
+            output = "Why " + str(canCapture.group(1)) + " you " + str(canCapture.group(2)).strip() + endingVerb + "?"
+        elif thoughtsCapture:
+            output = "Why do you think that?" if str(thoughtsCapture.group(1)) == "think" else "Why do you suppose that?"
+        elif examplesCapture:
+            output = "Can you give me a specific example?"
+        elif insultsCapture:
+            choices = ["Hey, no insults!", "Please be a little kinder!", "You will need to calm down a little."]
+            output = choices[random.randint(0, len(choices) - 1)]
+        else:
+            output = "Tell me more."
+
+        output = border + "\t" + bcolors.OKGREEN + output
+
+        print(output)
